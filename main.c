0e556e12b5cd112c31c57f197e52d922b147643f
kostr2010 <kostr2010@gmail.com>
2020-05-19 21:33:55 +0300

    major fixes

--- main.c
+++ main.c
@@ -2,23 +2,15 @@
 #include "./utils/log.h"
 
 int main() {
-  printf("hui\n");
-
-  //   Tree* tree = TreeRead("./input/database.txt");
-
-  //   TreeWrite(tree, "./output/tree.txt");
-
   Tree* tree = DiffReadExpression("input/expression.txt");
 
   DiffInitPdf();
+
   DiffPrintToPdf("d( ");
   DiffPrintTree(tree);
   DiffPrintToPdf(")/dx = ");
 
-  if (DiffGetDerivative(tree) == -1) {
-    printf("-1\n");
-    return -1;
-  }
+  DiffGetDerivative(tree);
 
   DiffPrintTree(tree);
   DiffClosePdf();
